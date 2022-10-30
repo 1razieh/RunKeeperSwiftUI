5005ba7acaf6b1f4f7c0da823aaf1a46a919747a
razieh <halimi.razieh@gmail.com>
2022-10-30 16:48:28 -0400

    Define space

--- RunKeeperSwiftUI/ContentView.swift
+++ RunKeeperSwiftUI/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
 //                        Section(header: Text(medal.categoryType)){
                             
                         
-                        VStack {
+                        VStack(spacing: 2) {
                             Image(medal.image)
                                 .resizable()
                                 .scaledToFit()
